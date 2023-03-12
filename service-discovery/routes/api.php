b2124697bc38ab3a49c3750af549b5302c95d7ea
Jesse Griffin <hemham914@gmail.com>
2023-03-12 03:48:29 -0700

    service-discovery default files

---
+++ service-discovery/routes/api.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+use Orion\Facades\Orion;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['as' => 'api.'], function() {
+    //Orion::resource('blirps', \App\Http\Controllers\Api\BlirpController::class);
+    Orion::resource('services', \App\Http\Controllers\Api\ServiceController::class);
+})->middleware('auth:sanctum');
+
+//Orion::resource('blirps', \App\Http\Controllers\Api\BlirpController::class);
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
